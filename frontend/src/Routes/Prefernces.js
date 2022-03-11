5e5d877b0d173bedcec3b88a06418ac26ac60a29
Priyanshu Yadav <yadavpriyanshu757@gmail.com>
2022-03-11 23:32:36 +0530

    Add Prescription added

--- frontend/src/Routes/Prefernces.js
+++ frontend/src/Routes/Prefernces.js
@@ -1,197 +1,135 @@
-import React from 'react';
-
-
-export default class DynamicTable extends React.Component {
-
-  constructor(props) {
-
-    super(props);
-
-
-    this.state = {
-
-      message: "",
-
-      items: []
-
+import React, {useEffect, useState} from 'react';
+import { Table, Button } from 'react-bootstrap';
+var i=0
+const DynamicTable = ({history}) =>{
+    var [items,setItems]=useState([])
+    var [itemsI,setItemsI]=useState([])
+    var [studentID,setStudentID]=useState(0)
+    var [doctorID,setDoctorID]=useState(0)
+    var [medicineID,setMedicineID]=useState(0)
+    var [testID,setTestID]=useState(0)
+    var [dose,setDose]=useState(0)
+    const addRowP=(e)=>{
+        e.preventDefault();
+        items=items.concat([{medicineID,dose}])
+        setItems(items)
+        i+=1
     }
-
-  }
-
-
-  updateMessage(event) {
-
-    this.setState({
-
-      message: event.target.value
-
-    });
-
-  }
-
-
-  handleClick() {
-
-    var items = this.state.items;
-
-
-    items.push(this.state.message);
-
-
-    this.setState({
-
-      items: items,
-
-      message: ""
-
-    });
-
-  }
-
-
-  handleItemChanged(i, event) {
-
-    var items = this.state.items;
-
-    items[i]  = event.target.value;
-
-
-    this.setState({
-
-      items: items
-
-    });
-
-  }
-
-
-  handleItemDeleted(i) {
-
-    var items = this.state.items;
-
-
-    items.splice(i, 1);
-
-
-    this.setState({
-
-      items: items
-
-    });
-
-  }
-
-
-  renderRows() {
-
-    var context = this;
-
-
-    return  this.state.items.map(function(o, i) {
-
-              return (
-
-                <tr key={"item-" + i}>
-
-                  <td>
-
-                    <input
-
-                      type="text"
-
-                      value={o}
-
-                      onChange={context.handleItemChanged.bind(context, i)}
-
-                    />
-
-                  </td>
-
-                  <td>
-
-                    <button
-
-                      onClick={context.handleItemDeleted.bind(context, i)}
-
-                    >
-
-                      Delete
-
-                    </button>
-
-                  </td>
-
-                </tr>
-
-              );
-
-            });
-
-  }
-
-
-  render() {
-
-    return (
-
-      <div>
-
-        <table className="">
-
-          <thead>
-
-            <tr>
-
-              <th>
-
-                Item
-
-              </th>
-
-              <th>
-
-                Actions
-
-              </th>
-
-            </tr>
-
-          </thead>
-
-          <tbody>
-
-            {this.renderRows()}
-
-          </tbody>
-
-        </table>
-
-        <hr/>
-
-        <input
-
-          type="text"
-
-          value={this.state.message}
-
-          onChange={this.updateMessage.bind(this)}
-
-        />
-
-        <button
-
-          onClick={this.handleClick.bind(this)}
-
-        >
-
-          Add Item
-
-        </button>
-
-      </div>
-
-    );
-
-  }
+    const addRowI=(e)=>{
+        e.preventDefault();
+        itemsI=itemsI.concat([{testID}])
+        setItemsI(itemsI)
+        i+=1
+    }
+    const deleteRow=(e,i)=>{
+        // console.log(i)
+        e.preventDefault();
+        items=items.concat([{medicineID,dose}])
+        setItems(items)
+    }
+    const addPrescription=(e)=>{
+        // e.preventDefault
+        return fetch('http://localhost:12345/addPrescription', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({"doctorID":doctorID,"studentID":studentID,"prescription_desc":items,"investigation":itemsI})
+        }).then(data => {
+            return data.json();
+        }).then((res)=>{
+            console.log(JSON.stringify(res))
+        })
+    }
+    return(
+        <>
+            <Table striped border hover responsive>
+                <thead>
+                    <tr>
+                        <th>MedicineID</th>
+                        <th>Dose</th>
+                        {/* <th></th> */}
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        items.map((row)=>{
+                            return(
+                                <tr key={i}>
+                                    <td>{row['medicineID']}</td>
+                                    <td>{row['dose']}</td>
+                                    {/* <td><Button type='submit' onClick={deleteRow(e,i)}>Delete</Button> </td> */}
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </Table>
+            <div className='form-wrapper'>
+                <form onSubmit={addRowP}>
+                    <label>
+                        <p>Set StudentID</p>
+                        <input type="number" onChange={e=>setStudentID(e.target.value)}/>
+                    </label>
+                    <label>
+                        <p>Set DoctorID</p>
+                        <input type="number" onChange={e=>setDoctorID(e.target.value)}/>
+                    </label>
+                    <h2>Enter Medicines</h2>
+                    <label>
+                        <p>MedicineID</p>
+                        <input type="text" onChange={e=>setMedicineID(e.target.value)}/>
+                    </label>
+                    <label>
+                        <p>Dose</p>
+                        <input type="number" onChange={e=>setDose(e.target.value)}/>
+                    </label>
+                    <div>
+                        <Button type='submit'>Add Medicine to prescription</Button>
+                    </div>
+                </form>
+            </div>
+            <Table striped border hover responsive>
+                <thead>
+                    <tr>
+                        <th>testID</th>
+                        {/* <th>Dose</th> */}
+                        {/* <th></th> */}
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        itemsI.map((row)=>{
+                            return(
+                                <tr key={i}>
+                                    <td>{row['testID']}</td>
+                                    {/* <td>{row['dose']}</td> */}
+                                    {/* <td><Button type='submit' onClick={deleteRow(e,i)}>Delete</Button> </td> */}
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </Table>
+            <div className='form-wrapper'>
+                <form onSubmit={addRowI}>
+                    <h2>Enter Tests</h2>
+                    <label>
+                        <p>TestID</p>
+                        <input type="text" onChange={e=>setTestID(e.target.value)}/>
+                    </label>
+                    <div>
+                        <Button type='submit'>Enter Test ID to add</Button>
+                    </div>
+                </form>
+            </div>
+            <div className='form-wrapper' onClick={addPrescription}>
+                <div>
+                    <Button type='submit'>Add Prescription</Button>
+                </div>
+            </div>
+        </>
+    )
 
 }
+export default DynamicTable
--- routes/addPrescription.js
+++ routes/addPrescription.js
@@ -15,26 +15,13 @@ addPrescription.use(bodyParser.urlencoded({extended:false}));
 
 addPrescription.post('/addPrescription', (req,res)=>{
     // data=JSON.parse(req.body);
+    data=req.body
+    console.log(data)
     async function add(data){
-        // tmp=await bcrypt.hash(data['password'],saltRounds);
-        // console.log(tmp);
-        // index.db.execute(
-        //     "INSERT INTO student VALUES(?,?,?,?,?)",[data['username'],tmp,data['name'],data['mobile'],data['address']],(err,result)=>{
-        //         if(err){
-        //             res.end("Something bad happened");
-        //         }
-        //         else{
-        //             res.end(result);
-        //         }
-        //     }
-        // ),(err,result)=>{
-        //     console.log(err)
-        // };
         index.db.connect((err)=>{
             if(err) throw err;
             else{
                 sqlQuery="INSERT INTO health.prescription(studentID,doctorID) VALUES(?)";
-                // values=[data['username'],tmp,data['name'],data['mobile'],data['address']];
                 values=[data["studentID"],data["doctorID"]]
                 index.db.query(sqlQuery,[values],(err,result)=>{
                     if(err) throw(err)
@@ -53,13 +40,14 @@ addPrescription.post('/addPrescription', (req,res)=>{
                         // console.log(typeof(prescriptionID))
                         // console.log(data["medicine"])
                         // console.log(data["medicine"])
-                        for (medicine in data["medicine"]){
-                            dose=data["medicine"][medicine]
+                        for (medicine in data["prescription_desc"]){
+                            dose=data["prescription_desc"][medicine]["dose"]
                             sqlQuery1="SELECT medicineID from health.medicine where `name`=?"
-                            values=[medicine]
+                            values=[data["prescription_desc"][medicine]["medicineID"]]
                             index.db.query(sqlQuery1,[values],(err,result)=>{
                                 if(err) throw(err)
                                 else {
+                                    i=0
                                     id=result[0]["medicineID"]
                                     console.log(id,dose)
                                     sqlQuery2="INSERT INTO health.prescription_desc(prescriptionID,medicineID,dose) VALUES(?)";
@@ -75,17 +63,22 @@ addPrescription.post('/addPrescription', (req,res)=>{
                             })
                         }
 
-                        for (test in data["test"]){
+                        for (test in data["investigation"]){
                             sqlQuery1="SELECT testID from health.test where `name`=?"
-                            values=[test]
+                            values=[data["investigation"][test]["testID"]]
+                            console.log(values)
                             index.db.query(sqlQuery1,[values],(err,result)=>{
                                 if(err) throw(err)
                                 else {
+                                    i=0
+                                    while(i<1000000)
+                                        i+=1
+                                    console.log(result)
                                     id=result[0]["testID"]
                                     console.log(id)
                                     sqlQuery2="INSERT INTO health.investigation VALUES(?)";
                                     // values=[data['username'],tmp,data['name'],data['mobile'],data['address']];
-                                    values=[prescriptionID,id,data["test"][test]]
+                                    values=[prescriptionID,id,""]
                                     index.db.query(sqlQuery2,[values],(err,result)=>{
                                         if(err) throw(err)
                                         else {
